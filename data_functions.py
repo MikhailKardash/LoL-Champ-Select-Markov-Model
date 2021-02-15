78184b45bc522f527ffa65391727906312eb5059
MikhailKardash <48943383+MikhailKardash@users.noreply.github.com>
2021-02-15 15:30:05 -0800

    Update main and data_functions
    updated main and data functions for pep8 compliance

--- data_functions.py
+++ data_functions.py
@@ -8,150 +8,178 @@ Created on Sat Nov 23 15:42:00 2019
 import numpy as np
 
 
-def run_alg(team_A_list,team_B_list,data,champ_inds,topN):
-    num = pick_num(team_A_list,team_B_list)
+def run_alg(team_a_list, team_b_list, data, champ_inds):
+    num = pick_num(team_a_list, team_b_list)
     flag = False
-    if not isAPicking(num,flag):
+    if not is_a_picking(num, flag):
         data = flipinds(data)
         flag = True
         print("Flipping data for B side")
-        temp = team_A_list.copy()
-        team_A_list = team_B_list.copy()
-        team_B_list = temp.copy()
-    winspace = create_constrained_space(data.copy(),team_A_list,team_B_list)
-    losspace = create_constrained_space(flipinds(data),team_A_list,team_B_list)
+        temp = team_a_list.copy()
+        team_a_list = team_b_list.copy()
+        team_b_list = temp.copy()
+    winspace = create_constrained_space(data.copy(),
+                                        team_a_list,
+                                        team_b_list)
+    losspace = create_constrained_space(flipinds(data),
+                                        team_a_list,
+                                        team_b_list)
     print(len(winspace))
     print(len(losspace))
-    champs = generate_champ_list(team_A_list,team_B_list,champ_inds)
-    prob = create_champ_probability(winspace,losspace,num,champs,team_A_list,team_B_list,champ_inds,flag)
-    prob_F = create_final_prob(prob)
-    #return get_amax(prob_F,topN)
-    return alternate_amax(prob_F,topN)
+    champs = generate_champ_list(team_a_list, team_b_list,
+                                 champ_inds)
+    prob = create_champ_probability(winspace, losspace,
+                                    num, champs,  champ_inds, flag)
+    final_prob = create_final_prob(prob)
+    return alternate_amax(final_prob)
+
 
 def flipinds(space):
     temp = np.asarray(space)
-    return np.stack([temp[:,1,:],temp[:,0,:]],1)
+    return np.stack([temp[:, 1, :], temp[:, 0, :]], 1)
 
-def pick_num(team_A_list,team_B_list):
-    num_picks = len(team_A_list) + len(team_B_list)
-    selNo= num_picks + 1
-    return selNo
 
-def create_constrained_space(space,team_A_list,team_B_list):
+def pick_num(team_a_list, team_b_list):
+    num_picks = len(team_a_list) + len(team_b_list)
+    selection_num = num_picks + 1
+    return selection_num
+
+
+def create_constrained_space(space, team_a_list, team_b_list):
     out = space
-    if len(team_A_list) > 0:
-        for A in team_A_list:
+    if len(team_a_list) > 0:
+        for A in team_a_list:
             out = [el for el in out if A in el[0]]
-    if len(team_B_list) > 0:
-        for B in team_B_list:
+    if len(team_b_list) > 0:
+        for B in team_b_list:
             out = [el for el in out if B in el[1]]
     return out
 
-def generate_champ_list(team_A_list,team_B_list,champ_inds):
-    A = champ_inds.copy()
-    A = [el for el in A if el not in team_A_list]
-    return [el for el in A if el not in team_B_list]
 
-def get_amax(probs,topN):
+def generate_champ_list(team_a_list, team_b_list, champ_inds):
+    valid = champ_inds.copy()
+    valid = [el for el in valid if el not in team_a_list]
+    return [el for el in valid if el not in team_b_list]
+
+
+def get_amax(probs, top_n):
     temp = np.asarray(probs)
-    inds = np.argsort(temp[:,1]).tolist()
-    champs = temp[inds[-topN:],0]
-    probs = temp[inds[-topN:],1]
+    inds = np.argsort(temp[:, 1]).tolist()
+    champs = temp[inds[-top_n:], 0]
+    probs = temp[inds[-top_n:], 1]
     return [np.flip(champs), np.flip(probs)]
 
-def alternate_amax(probs,topN):
+
+def alternate_amax(probs):
     temp = np.asarray(probs)
-    p_vec = np.unique(temp[:,1])
+    p_vec = np.unique(temp[:, 1])
     p_vec = np.flip(np.sort(p_vec))
-    inds_final = [];
+    inds_final = []
     for el in p_vec:
-        inds = np.argwhere(temp[:,1] == el) #find inds of probs.
+        # find inds of probs.
+        inds = np.argwhere(temp[:, 1] == el)
         inds = np.squeeze(inds)
         inds = inds.tolist()
-        temp2 = temp[inds,:] #isolate probs we're interested in
-        if isinstance(inds,int):
+        # isolate probs we're interested in
+        temp2 = temp[inds, :]
+        if isinstance(inds, int):
             inds_final = [*inds_final, inds]
         else:
-            temp2 = np.reshape(temp2,[len(inds),temp2.shape[-1]])
-            inds_F = np.argsort(temp2[:,2]) #sort new data by win number
-            inds_F = np.flip(inds_F).tolist() #get in decreasing order
+            temp2 = np.reshape(temp2, [len(inds), temp2.shape[-1]])
+            # sort new data by win number
+            inds_final = np.argsort(temp2[:, 2])
+            # get in decreasing order
+            inds_final = np.flip(inds_final).tolist()
             inds = np.asarray(inds)
-            inds_final = [*inds_final, *inds[inds_F]]
+            inds_final = [*inds_final, *inds[inds_final]]
     out = temp[inds_final]
-    return [out[:,0],out[:,1],out[:,2]]
-        
+    return [out[:, 0], out[:, 1], out[:, 2]]
         
 
-def get_winloss(winspace,lossspace,champ_list,flag):
-    out = [];
+def get_winloss(winspace, lossspace, champ_list, flag):
+    out = []
     for num in champ_list:
-        #last pick is always team B.
+        # last pick is always team B.
         if not flag:
             win_games = [el for el in winspace if num in el[1]]
             loss_games = [el for el in lossspace if num in el[1]]
         else:
             win_games = [el for el in winspace if num in el[0]]
             loss_games = [el for el in lossspace if num in el[0]]
-        out.append([num,len(win_games),len(loss_games)])
+        out.append([num, len(win_games), len(loss_games)])
     return np.asarray(out)
-    
+
+
 def marginalize(probs):
     temp = np.asarray(probs)
-    wins = np.sum(temp[:,1])
-    losses = np.sum(temp[:,2])
-    #normalization = wins + losses
-    #if normalization == 0:
-    #    return [0,0]
-    #else:
-    #    return [wins/normalization, losses/normalization]
-    return [wins,losses]
+    wins = np.sum(temp[:, 1])
+    losses = np.sum(temp[:, 2])
+    # normalization code left if necessary.
+    '''
+    normalization = wins + losses
+    if normalization == 0:
+        return [0,0]
+    else:
+        return [wins/normalization, losses/normalization]
+    '''
+    return [wins, losses]
 
-def isAPicking(numb,flag):
+
+def is_a_picking(numb, flag):
     if (numb is 1) or (numb is 4) or (numb is 5) or (numb is 8) or (numb is 9):
         return not flag
     else:
         return flag
-    
+
+
 def create_final_prob(inmat):
-    out = [];
+    out = []
     for el in inmat:
         if (el[1] + el[2]) > 0:
             numgames = el[1] + el[2]
-            winrate = el[1]/(numgames);
+            winrate = el[1]/numgames
         else:
-            numgames = 0;
-            winrate = 0;
-        out.append([el[0],winrate,numgames])
+            numgames = 0
+            winrate = 0
+        out.append([el[0], winrate, numgames])
     return out
-    
-def create_champ_probability(winspace,lossspace,num,champ_list,team_A_list,team_B_list,champ_inds,flag):
+
+
+def create_champ_probability(winspace, lossspace,
+                             num, champ_list,
+                             champ_inds, flag):
     if num is 10:
-        return get_winloss(winspace,lossspace,champ_list,flag)
+        return get_winloss(winspace, lossspace,
+                           champ_list, flag)
     elif num is 11:
         return 0
     else:
-        out = [];
+        out = []
         for champ in champ_list:
-            if isAPicking(num,flag):
-                newA = [champ]
-                newwin = create_constrained_space(winspace,newA,[])
-                newloss = create_constrained_space(lossspace,newA,[])
+            if is_a_picking(num, flag):
+                new_a = [champ]
+                newwin = create_constrained_space(winspace, new_a, [])
+                newloss = create_constrained_space(lossspace, new_a, [])
                 if len(newwin) is 0 and len(newloss) is 0:
-                    out.append([champ,0,0])
+                    out.append([champ, 0, 0])
                 else:
-                    newchamps = generate_champ_list(newA,[],champ_list)
-                    temp = create_champ_probability(newwin,newloss,num+1,newchamps,newA,[],champ_inds,flag)
+                    newchamps = generate_champ_list(new_a, [], champ_list)
+                    temp = create_champ_probability(newwin, newloss,
+                                                    num+1, newchamps,
+                                                    champ_inds, flag)
                     marg = marginalize(temp)
-                    out.append([champ,marg[0],marg[1]])   
+                    out.append([champ, marg[0], marg[1]])
             else:
-                newB = [champ]
-                newwin = create_constrained_space(winspace,[],newB)
-                newloss = create_constrained_space(lossspace,[],newB)
+                new_b = [champ]
+                newwin = create_constrained_space(winspace, [], new_b)
+                newloss = create_constrained_space(lossspace, [], new_b)
                 if len(newwin) is 0 and len(newloss) is 0:
-                    out.append([champ,0,0])
+                    out.append([champ, 0, 0])
                 else:    
-                    newchamps = generate_champ_list([],newB,champ_list)
-                    temp = create_champ_probability(newwin,newloss,num+1,newchamps,[],newB,champ_inds,flag)
+                    newchamps = generate_champ_list([], new_b, champ_list)
+                    temp = create_champ_probability(newwin, newloss,
+                                                    num+1, newchamps,
+                                                    champ_inds, flag)
                     marg = marginalize(temp)
-                    out.append([champ,marg[0],marg[1]])      
+                    out.append([champ, marg[0], marg[1]])
         return np.asarray(out)
--- main.py
+++ main.py
@@ -5,28 +5,35 @@ Created on Wed Dec  4 17:17:35 2019
 @author: Michael K
 """
 
-import numpy as np
 import pandas as pd
 from data_functions import *
 import time
 
 start_time = time.time()
 
+# load data
 data = np.load('datamap_file.npy')
 
-team_A_picks = [72];
-team_B_picks = [34];
+# try these two picks
+team_A_picks = [72]
+team_B_picks = [34]
 
 
-#team_A_picks = [21];
-#team_B_picks = [92,51];
+# team_A_picks = [21]
+# team_B_picks = [92,51]
 
+# this is to associate string with int
 champ_info = pd.read_csv("champs.csv")
 info = champ_info.values
-champ_inds = info[:,1].astype("int64")
+champ_inds = info[:, 1].astype("int64")
 
+# run alg
+[champs, probs, games] = run_alg(team_A_picks,
+                                 team_B_picks,
+                                 data,
+                                 champ_inds)
 
-[champs,probs,games] = run_alg(team_A_picks,team_B_picks,data,champ_inds,60)
+# print results
 print(champs)
 print(probs)
 print(games)
